edd2555eacb3d12c3d4bea8112445b3a46deee5a
Кирилл Викулин <k.vikulin@inventos.ru>
2024-09-30 16:25:00 +0300

    mv

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,20 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_25_201002) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  create_table "follows", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,4 +56,5 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_25_201002) do
   end
 
   add_foreign_key "comments", "articles"
+  add_foreign_key "posts", "users"
 end
